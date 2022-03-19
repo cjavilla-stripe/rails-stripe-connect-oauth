b1b746952cc711ad61d8b62b651dd09e0906592f
CJ Avilla <cjavilla@stripe.com>
2022-03-18 20:00:56 -0400

    Rails app with Stripe Connect OAuth

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    omniauth_callbacks: "omniauth_callbacks"
+  }
+  get '/connect/callback', to: 'omniauth_callbacks#stripe_connect'
 end
